12b8280f892c59a778f074f9e2114552043a50f4
Iman <ostovar0074@gmail.com>
2021-01-31 22:47:37 +0100

    Lab 1 report part 2

--- Lab1/DataAnylyser.m
+++ Lab1/DataAnylyser.m
@@ -1,13 +1,13 @@
 %%
 clc
 %clear all
-%z = cell(7,3)
+%z = cell(7,4)
 
 clearvars -except sortedFileContents z
 
 
 idleTimout = sortedFileContents(:,1);
-%sleepTimeout = sortedFileContents(:,2);
+sleepTimeout = sortedFileContents(:,2);
 % activeTime = sortedFileContents(:,3);
 % idleTime= sortedFileContents(:,4);
 % totalTime = sortedFileContents(:,5);
@@ -15,18 +15,21 @@ idleTimout = sortedFileContents(:,1);
 % timeInActive = sortedFileContents(:,7);
 % timeInIdle = sortedFileContents(:,8);
 % timeInSleep = sortedFileContents(:,9);
-%timeOverHead = sortedFileContents(:,10);
+% timeOverHead = sortedFileContents(:,10);
 % numOfTrans = sortedFileContents(:,11);
 energyOfTrans = sortedFileContents(:,12);
 % energyWithTrans = sortedFileContents(:,13);
 % energyWithoutTrans = sortedFileContents(:,14);
 energySavedPercent = sortedFileContents(:,15);
 
-z(7,:) = {idleTimout, energyOfTrans, energySavedPercent};
+z(7,:) = {idleTimout, energyOfTrans, energySavedPercent,sleepTimeout};
+clearvars -except z
 %%
-baseAddress = "dpm_simulator/example/Mdata/only idle/Results/";
-save (baseAddress+"Res_T_idleTimout",'z');
-%%
+baseAddress = "dpm_simulator/example/Mdata/idle and sleep/Results/";
+z2=z;
+save (baseAddress+"Res_T_idleTimout",'z2');
+clearvars -except z baseAddress
+%% 2D
 close all
 %plot(idleTimout,energySavedPercent);
 figure
@@ -44,7 +47,7 @@ plot(cell2mat(z(7,1)),cell2mat(z(7,2)),'s-k','LineWidth', linewidth);
 
 legend ('custom_1','custom_2','Uniform_1', 'Uniform_2','Normall','Exponential','Tri-modal')
 
-fname = sprintf(baseAddress+ 'Res_T_idleTimout(time Over Head)');
+fname = sprintf(baseAddress+ 'Res_T_idlesleepTimout(Energy Overhead)');
 set(gcf, 'Units', 'Normalized','OuterPosition', [0 0 1 1]);
 saveas(gcf,fname,'png')
 saveas(gcf,fname)
@@ -52,6 +55,7 @@ saveas(gcf,fname)
 %
 figure
 plot(cell2mat(z(1,1)),cell2mat(z(1,3)),'x-b','LineWidth', linewidth);
+
 xlabel('Idle Timout')
 ylabel('Energy Saved Percent')
 
@@ -65,7 +69,7 @@ plot(cell2mat(z(7,1)),cell2mat(z(7,3)),'s-k','LineWidth', linewidth);
 
 legend ('custom_1','custom_2','Uniform_1', 'Uniform_2','Normall','Exponential','Tri-modal')
 
-fname = sprintf(baseAddress+ 'Res_T_idleTimout(Energy Saved Percent)');
+fname = sprintf(baseAddress+ 'Res_T_idlesleepTimout(Energy Saved Percent)');
 set(gcf, 'Units', 'Normalized','OuterPosition', [0 0 1 1]);
 saveas(gcf,fname,'png')
 saveas(gcf,fname)
@@ -74,7 +78,69 @@ saveas(gcf,fname)
 %plot(x,y1,'g',x,y2,'b--o',x,y3,'c*')
 
 %close all
+%% 3D
+close all
+figure
+xaxis=2;
+yaxis=4;
+
+subplot(xaxis,yaxis,1)
+    plotwhole(1,z);
+    title('1. Custom #1')
+subplot(xaxis,yaxis,2)
+    plotwhole(2,z);
+    title('2. Custom #2')
+subplot(xaxis,yaxis,3)
+    plotwhole(3,z);
+    title('3. Idle Uniform #1')
+subplot(xaxis,yaxis,4)
+    plotwhole(4,z);
+    title('4. Idle Uniform #2')
+subplot(xaxis,yaxis,5)
+    plotwhole(5,z);
+    title('5. Idle Normal')
+subplot(xaxis,yaxis,6)
+    plotwhole(6,z);
+    title('6. Idle Exponential')
+subplot(xaxis,yaxis,7)
+    plotwhole(7,z);
+    title('7. Idle Tri-modal')
+    
+fname = sprintf(baseAddress+ 'Res_T_idlesleepTimout (Energy Overhead)');
+set(gcf, 'Units', 'Normalized', 'OuterPosition', [0 0 1 1]);
+saveas(gcf,fname,'png')
+saveas(gcf,fname)
+
+
 
 %%
 clearvars -except sortedFileContents
 close all
+function []=plotwhole (Num,z)
+    %Num = 2;
+    x= (cell2mat(z(Num,1)));
+    s= (cell2mat(z(Num,4)));
+    y = (cell2mat(z(Num,2)));
+    x_old = x(1);
+    index= 1 ;
+    i=1;
+    while (i <= size(x,1))
+        if (x(i) ~= x_old)
+            x_old = x(i);
+            index = [index,i];
+        end
+        i=i+1;
+    end
+    for i = 1:size(index,2)-1
+        y_l = y(index(i):index(i+1)-1);
+        s_l = s(index(i):index(i+1)-1);
+        if (~isempty (find(y_l > y_l(end))))
+            i
+        end
+        plot(s_l,y_l);
+        xlabel('Sleep Timout')
+        ylabel('Energy Overhead')
+        hold on
+    end
+    hold off
+end
